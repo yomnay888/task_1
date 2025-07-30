4b4aacf1643ecfdd5f76075eb36ae9a0a8d62b19
Yomna Yasser <yomnay888@gmail.com>
2025-07-30 13:15:50 +0300

    adding .env for all

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,10 @@ WORKDIR /app
 # Copy the jar file into the container
 COPY target/task_1-1.0-SNAPSHOT-jar-with-dependencies.jar app.jar
 
+# COPY .ENV
+COPY .env src/main/resources/.env
+
+
 # Expose any port your app uses (optional, only if needed)
 # EXPOSE 8080
 
--- src/main/java/org/example/HibernateUtil.java
+++ src/main/java/org/example/HibernateUtil.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -8,9 +9,29 @@ public class HibernateUtil {
 
     static {
         try {
-            sessionFactory = new Configuration()
-                    .configure("hibernate.cfg.xml")
-                    .buildSessionFactory();
+            Dotenv dotenv = Dotenv.load();
+
+            String dbUrl = String.format(
+                    "jdbc:mysql://%s:%s/%s",
+                    dotenv.get("DB_HOST"),
+                    dotenv.get("DB_PORT"),
+                    dotenv.get("DB_NAME")
+            );
+
+            Configuration configuration = new Configuration();
+            configuration.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+            configuration.setProperty("hibernate.connection.url", dbUrl);
+            configuration.setProperty("hibernate.connection.username", dotenv.get("DB_USER"));
+            configuration.setProperty("hibernate.connection.password", dotenv.get("DB_PASSWORD"));
+            configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+            configuration.setProperty("hibernate.hbm2ddl.auto", "update");
+            configuration.setProperty("show_sql", "true");
+
+            configuration.addAnnotatedClass(org.example.User.class);
+            configuration.addAnnotatedClass(org.example.Book.class);
+            configuration.addAnnotatedClass(org.example.BorrowedBook.class);
+
+            sessionFactory = configuration.buildSessionFactory();
         } catch (Throwable ex) {
             throw new ExceptionInInitializerError(ex);
         }
